5252d716a1ebf5acde3527a7f56b8de209515a78
Vahlgram <yardbird.is.in@gmail.com>
2022-04-25 17:45:22 +0800

    Rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x3AF172ef5e0c62ADf257Ffa19381e18805A7ed26"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Ragnarok is upon us!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = "As we both know Souls of warriors dont go to heaven,they are summoned to the halls of Valhalla to fight for another day.	Ragnarok isnt an event marking the end. It's a fist fight battle royale where we Lords wager on whose battler survives.";
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
