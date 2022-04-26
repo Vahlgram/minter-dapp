29437e280e35ab089a2b9c7b419863820170e690
Vahlgram <yardbird.is.in@gmail.com>
2022-04-26 21:43:17 +0800

    Change

--- backend/src/config.js
+++ backend/src/config.js
@@ -38,7 +38,7 @@ const format = {
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://www.facebook.com/gaming/OraOraBattler", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
